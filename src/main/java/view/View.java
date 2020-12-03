aba13fbe09c53aef859b692e329089eda9d723f2
Aleksandra Cynk <305284@student.agh.edu.pl>
2020-12-03 20:14:57 +0100

    Map icon-images

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -2,6 +2,8 @@ package view;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -9,6 +11,12 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import static view.Element.DALEK;
 
 public class View {
 
@@ -21,11 +29,11 @@ public class View {
     private final int fieldSize = 60;
     private final int fieldGap = 2;
 
-    private final float doctorSize = 50;
-    private final Color doctorColor = Color.BLUE;
+    private final String doctorImageSrc = "src/images/doctor.png";
+    private final float doctorSize = 60;
 
-    private final float dalekSize = 40;
-    private final Color dalekColor = Color.RED;
+    private final String dalekImageSrc = "src/images/dalek.png";
+    private final float dalekSize = 60;
 
     private final float junkSize = fieldSize;
     private final Color junkColor = Color.DARKGRAY;
@@ -64,24 +72,49 @@ public class View {
     }
 
     public void paintElement(Element elementType, int gridX, int gridY){
-        float elementSize;
-        Color elementColor;
+        Boolean pictureType = true;
+
+        float elementSize = 0;
+        String elementImageSrc = null;
+        Color elementColor = null;
+
         switch(elementType) {
             case DOCTOR:
+                elementImageSrc = doctorImageSrc;
                 elementSize = doctorSize;
-                elementColor = doctorColor;
                 break;
             case DALEK:
+                elementImageSrc = dalekImageSrc;
                 elementSize = dalekSize;
-                elementColor = dalekColor;
                 break;
             default:
+                pictureType = false;
+        }
+        switch(elementType) {
+            case JUNK:
                 elementSize = junkSize;
                 elementColor = junkColor;
         }
-        Shape element = new Circle(calculateElementPosition(gridX, false), calculateElementPosition(gridY, true), elementSize/2, elementColor);
-        root.getChildren().add(element);
+            if (pictureType == false) {
+                Shape element = new Circle(calculateElementPosition(gridX, false), calculateElementPosition(gridY, true), elementSize / 2,  elementColor);
+                root.getChildren().add(element);
+            }
+            
+            if (pictureType == true){
+                Image image = null;
+                try {
+                    image = new Image(new FileInputStream(elementImageSrc));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                ImageView imageView1 = new ImageView(image);
+                imageView1.setY(calculateImageElementPosition(gridY, elementSize, true));
+                imageView1.setX(calculateImageElementPosition(gridX, elementSize, false));
 
+                imageView1.setFitHeight(elementSize);
+                imageView1.setFitWidth(elementSize);
+                root.getChildren().add(imageView1);
+            }
     }
 
     public void setParameters(int worldWidth, int worldHeight){
@@ -100,5 +133,10 @@ public class View {
     private double calculateElementPosition(int gridPosition, boolean yAxis){
         if(yAxis) gridPosition = worldHeight - gridPosition + 1;
         return gridPosition*fieldSize - (double) fieldSize/2 + (gridPosition-1)*fieldGap;
+    }
+
+    private double calculateImageElementPosition(int gridPosition, float picSize, boolean yAxis){
+        if(yAxis) gridPosition = worldHeight - gridPosition + 1;
+        return gridPosition*fieldSize - fieldSize + (gridPosition - 1)*fieldGap + (fieldSize - picSize)/2;
     }
 }
